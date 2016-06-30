516a089e89337b7c2147f666d48cedd9d0ff9644
moxuyou <moxuyou@moxuyoudemac-mini.local>
2016-06-30 10:05:04 +0800

    添加注释

--- LXHPersonMessage/LXHPersonMessage/LXHPersonMessageViewController.m
+++ LXHPersonMessage/LXHPersonMessage/LXHPersonMessageViewController.m
@@ -11,13 +11,6 @@
 #import "LXHPersonSettingTableViewController.h"
 #import "Masonry.h"
 
-typedef enum {
-    BtnClickTypePerson = 0,
-    BtnClickTypeDetail,
-    BtnClickTypePersonSetting
-} BtnClickType;
-
-
 @interface LXHPersonMessageViewController ()<UITableViewDelegate,UITableViewDataSource>
 
 /* 装住大imageView的底部View */
@@ -49,11 +42,11 @@ typedef enum {
 /** 当前选中显示的tableView */
 @property (nonatomic , weak)UITableView *curronTableView;
 
-
+/** 用于记录当前背景scrollView的滚动清空 */
 @property (nonatomic , assign)CGFloat curronY;
+/** 用于记录三个scrollView的偏移量 */
 @property (nonatomic , strong)NSMutableDictionary *contentOffsets;
-@property (nonatomic , assign)BtnClickType ClickType;
-@property (nonatomic , assign)BOOL isScrolling;
+
 @end
 
 @implementation LXHPersonMessageViewController
@@ -65,14 +58,14 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
 - (NSMutableDictionary *)contentOffsets {
     if (!_contentOffsets) {
         _contentOffsets = [NSMutableDictionary dictionary];
-        _contentOffsets[personOffsetKey] = @(-108);
-        _contentOffsets[detailOffsetKey] = @(-108);
-        _contentOffsets[personSettingOffsetKey] = @(-108);
+//        _contentOffsets[personOffsetKey] = @(-244);
+//        _contentOffsets[detailOffsetKey] = @(-244);
+//        _contentOffsets[personSettingOffsetKey] = @(-244);
     }
     return _contentOffsets;
 }
 
-//懒加载创建personTableView
+//懒加载创建personTableView（个人信息）
 - (UITableView *)personTableView
 {
     if (_personTableView == nil) {
@@ -84,7 +77,7 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
     }
     return _personTableView;
 }
-
+//懒加载创建detailTableView（详细信息）
 - (UITableView *)detailTableView
 {
     if (_detailTableView == nil) {
@@ -98,7 +91,7 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
     }
     return _detailTableView;
 }
-
+//懒加载创建personSettingTableView（个人设置）
 - (UITableView *)personSettingTableView
 {
     if (_personSettingTableView == nil) {
@@ -112,7 +105,7 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
     }
     return _personSettingTableView;
 }
-
+//创建界面切换的导航栏（三个按钮那个父View）
 - (UIView *)footView
 {
     if (_footView == nil) {
@@ -129,28 +122,32 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.isScrolling = NO;
-    
+    //设置当前tableView为个人信息
     self.curronTableView = self.personTableView;
+    //初始化当前整个bgScrollView的偏移量
     self.curronY = self.curronTableView.contentOffset.y;
     self.contentOffsets[personOffsetKey] = @(self.curronTableView.contentOffset.y);
-    
+    //禁止默认偏移
     self.automaticallyAdjustsScrollViewInsets = NO;
-    self.curronTableView.dataSource = self;
-    self.curronTableView.delegate = self;
-    
+
+    self.personTableView.dataSource = self;
+    self.personTableView.delegate = self;
+    //设置导航栏透明
     self.navigationController.navigationBar.alpha = 0;
+    //默认点击个人信息按钮初始化
     [self personButtonClick:self.personButton];
     
-    self.curronTableView.contentInset = UIEdgeInsetsMake(self.bgView.bounds.size.height + 44, 0, 0, 0);
-    [self.curronTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
-    
+    self.personTableView.contentInset = UIEdgeInsetsMake(self.bgView.bounds.size.height + 44, 0, 0, 0);
+    [self.personTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    //监听详情信息和个人信息页面的滚动
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(messageDidScroll:) name:@"messageDidScroll" object:nil];
+    //监听详情信息和个人信息页面的惯性滚动
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(messageDidScrolling:) name:@"messageDidScrolling" object:nil];
+    //详情信息和个人信息页面的一下子停止滚动
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(messageDidScrolled:) name:@"messageDidScrolled" object:nil];
     
 }
-//[[NSNotificationCenter defaultCenter] postNotificationName:@"messageDidScrolling" object:self userInfo:@{@"messageDidScrolling" : scrollView,@"decelerate" : @"0"}];
+//监听详情信息和个人信息页面的惯性滚动
 - (void)messageDidScrolling:(NSNotification *)note
 {
     UIScrollView *scrollView = note.userInfo[@"messageDidScrolling"];
@@ -159,27 +156,30 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
     [self scrollViewDidEndDragging:scrollView willDecelerate:decelerate];
     
 }
+//详情信息和个人信息页面的一下子停止滚动
 - (void)messageDidScrolled:(NSNotification *)note
 {
     UIScrollView *scrollView = note.userInfo[@"messageDidScrolled"];
     [self scrollViewDidEndDecelerating:scrollView];
     
 }
+//监听详情信息和个人信息页面的滚动
 - (void)messageDidScroll:(NSNotification *)note
 {
     UIScrollView *scrollView = note.userInfo[@"messageDidScroll"];
     [self scrollViewDidScroll:scrollView];
     
 }
-
+//对象销毁前做的事情
 - (void)dealloc
 {
+    //移除通知
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 50;
+    return 100;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -195,13 +195,13 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
     return cell;
 }
 
-
+//监听当前显示控制器的惯性滚动停止
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
 {
     NSLog(@"惯性导致的滚动停止了");
     self.headView.userInteractionEnabled = YES;
 }
-
+//监听当前显示控制器的惯性滚动和一下子停止
 - (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
 {
     if (decelerate) {
@@ -213,12 +213,12 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
     }
     
 }
-
+//监听当前控制器的滚动
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-    
+    //获取当前偏移量
     CGFloat offset = self.curronTableView.contentInset.top +self.curronTableView.contentOffset.y;
-//    NSLog(@"%lf", offset);
+    //控制偏移量的最小值不能小于导航栏的高度+headView的高度
     if (offset > (self.curronTableView.contentInset.top - self.headView.bounds.size.height - 64)) {
         offset = self.curronTableView.contentInset.top - self.headView.bounds.size.height - 64;
     }
@@ -227,26 +227,23 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
     self.bgImageViewConstraint.constant = 200 - offset;
 
     //控制导航条颜色
-//    NSLog(@"%lf", 1.0 * offset / (self.tableView.contentInset.top - 64 - 44));
     self.navigationController.navigationBar.alpha = 1.0 * offset / (self.curronTableView.contentInset.top - self.headView.bounds.size.height - 64);
-
+    //判断设置当前显示控制器的偏移量
     if (scrollView.frame.origin.x == 0) {
         self.contentOffsets[personOffsetKey] = @(self.curronTableView.contentOffset.y);
     
     } else if (scrollView.frame.origin.x == self.view.bounds.size.width) {
         
-//        NSLog(@"detailOffsetKey%f",self.curronTableView.contentOffset.y);
-        
         self.contentOffsets[detailOffsetKey] = @(self.curronTableView.contentOffset.y);
     } else {
-        
-//        NSLog(@"personSettingOffsetKey%f",self.curronTableView.contentOffset.y);
         self.contentOffsets[personSettingOffsetKey] = @(self.curronTableView.contentOffset.y);
     }
     
 }
-
+//个人信息按钮点击
 - (IBAction)personButtonClick:(UIButton *)btn {
+//    [self setUpCurronTableView:self.personTableView contentOffsets:personOffsetKey clickBtn:btn];
+//    headView底部线的滑动
     [UIView animateWithDuration:0.25 animations:^{
         CGRect frame = self.footView.frame;
         frame.size.width = btn.titleLabel.frame.size.width;
@@ -254,15 +251,14 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
         self.footView.center = CGPointMake(btn.center.x, self.footView.center.y);
         self.bgScrollView.contentOffset = CGPointMake(0, 0);
     }];
-    
-    self.ClickType = BtnClickTypePerson;
-    
+
+    //切换控制器的View
     self.curronTableView = self.personTableView;
-    
+    //获取自己的tableView偏移量
     CGFloat offsetY = [self.contentOffsets[personOffsetKey] doubleValue];
-    
+    //根据当前整个偏移量设置每个tableView的偏移量
     if (self.curronY >= -108) {
-        
+        //判断每个子控制器tableView的偏移量，如果大于-108则使用自己的（上面），小于-108（下面）
         if (offsetY >= -108) {
             
             [self.curronTableView setContentOffset:CGPointMake(0, offsetY)];
@@ -274,13 +270,13 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
     } else {
         [self.curronTableView setContentOffset:CGPointMake(0, self.curronY)];
     }
-    
-//    CGPoint offset = self.curronTableView.contentOffset;
-//    (self.curronTableView.contentOffset.y > 0) ? offset.y-- : offset.y++;
-//    [self.curronTableView setContentOffset:offset animated:YES];
+//    保存备份，用于控制scrollView停止滚动的方法
+    CGPoint offset = self.curronTableView.contentOffset;
+    (self.curronTableView.contentOffset.y > 0) ? offset.y-- : offset.y++;
+    [self.curronTableView setContentOffset:offset animated:YES];
 }
 - (IBAction)detailButtonClick:(UIButton *)btn {
-    
+//    [self setUpCurronTableView:self.detailTableView contentOffsets:detailOffsetKey clickBtn:btn];
     [UIView animateWithDuration:0.25 animations:^{
         CGRect frame = self.footView.frame;
         frame.size.width = btn.titleLabel.frame.size.width;
@@ -289,7 +285,6 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
         self.bgScrollView.contentOffset = CGPointMake(self.view.bounds.size.width, 0);
     }];
     
-    self.ClickType = BtnClickTypeDetail;
     self.curronTableView = self.detailTableView;
     CGFloat offsetY = [self.contentOffsets[detailOffsetKey] doubleValue];
     
@@ -309,6 +304,8 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
     
 }
 - (IBAction)personSettingButtonClick:(UIButton *)btn {
+//    [self setUpCurronTableView:self.personSettingTableView contentOffsets:personSettingOffsetKey clickBtn:btn];
+    
     [UIView animateWithDuration:0.25 animations:^{
         CGRect frame = self.footView.frame;
         frame.size.width = btn.titleLabel.frame.size.width;
@@ -317,7 +314,6 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
         self.bgScrollView.contentOffset = CGPointMake(self.view.bounds.size.width * 2, 0);
     }];
     
-    self.ClickType = BtnClickTypePersonSetting;
     self.curronTableView = self.personSettingTableView;
     CGFloat offsetY = [self.contentOffsets[personSettingOffsetKey] doubleValue];
     
@@ -336,5 +332,40 @@ NSString *const personSettingOffsetKey = @"personSettingOffsetKey";
     }
 }
 
+- (void)setUpCurronTableView:(UITableView *)curronTableView contentOffsets:(NSString *)contentOffsetskey clickBtn:(UIButton *)clickBtn
+{
+    //headView底部线的滑动
+//    [UIView animateWithDuration:0.25 animations:^{
+//        CGRect frame = self.footView.frame;
+//        frame.size.width = clickBtn.titleLabel.frame.size.width;
+//        self.footView.frame = frame;
+//        self.footView.center = CGPointMake(clickBtn.center.x, self.footView.center.y);
+//        self.bgScrollView.contentOffset = CGPointMake(0, 0);
+//    }];
+//    
+//    //切换控制器的View
+//    self.curronTableView = curronTableView;
+//    //获取自己的tableView偏移量
+//    CGFloat offsetY = [self.contentOffsets[contentOffsetskey] doubleValue];
+//    //根据当前整个偏移量设置每个tableView的偏移量
+//    if (self.curronY >= -108) {
+//        //判断每个子控制器tableView的偏移量，如果大于-108则使用自己的（上面），小于-108（下面）
+//        if (offsetY >= -108) {
+//            
+//            [self.curronTableView setContentOffset:CGPointMake(0, offsetY)];
+//        } else {
+//            
+//            [self.curronTableView setContentOffset:CGPointMake(0, -108)];
+//        }
+//        
+//    } else {
+//        [self.curronTableView setContentOffset:CGPointMake(0, self.curronY)];
+//    }
+    
+//    保存备份，用于控制scrollView停止滚动的方法
+//        CGPoint offset = self.curronTableView.contentOffset;
+//        (self.curronTableView.contentOffset.y > 0) ? offset.y-- : offset.y++;
+//        [self.curronTableView setContentOffset:offset animated:YES];
+}
 
 @end
